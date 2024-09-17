bb7b006dc42b66033cc89e3c1cbc5654896e1490
Mathew <mathewburden.com>
2024-09-17 14:05:23 +0100

    added george

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("George");)
+        System.out.println(sayHelloToSomeone("George"));
         System.out.println(sayGoodbye());
     }
 
